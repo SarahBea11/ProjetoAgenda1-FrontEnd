4557add8f2fb1c298792e5bb098a5f123f5552f3
SarahBea11 <sarahbealimapina@gmail.com>
2024-12-09 17:06:04 -0300

    Criando rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AgendaComponent } from "./agenda/agenda.component";
+import { InicioComponent } from "./inicio/inicio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AgendaComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'agenda', component: AgendaComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
